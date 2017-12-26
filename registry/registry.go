7e49b83bd343f73957a7155df96f750a246998c7
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2017-12-26 17:40:13 +0100

    ignore tag deletion error

--- registry/registry.go
+++ registry/registry.go
@@ -19,22 +19,24 @@ func checkMatch(matches []string, name string) bool {
 	return false
 }
 
-func makeError(msg string) {
+func makeError(msg string, exitCode int) {
 	fmt.Println(msg)
-	os.Exit(1)
+
+	if exitCode > 0 {
+		os.Exit(exitCode)
+	}
 }
 
 func CleanTags(url string, repo string, km []string, username string, password string) {
 
 	hub, err := registry.New(url, username, password)
-
 	if err != nil {
-		makeError("Unable to connect to registry")
+		makeError("Unable to connect to registry", 1)
 	}
 
 	tags, err := hub.Tags(repo)
 	if err != nil {
-		makeError("Unable to get tags")
+		makeError("Unable to get tags", 2)
 	}
 
 	for i := 0; i < len(tags); i++ {
@@ -45,13 +47,13 @@ func CleanTags(url string, repo string, km []string, username string, password s
 			fmt.Printf(" DELETE\n")
 			digest, err := hub.ManifestDigest(repo, tag)
 			if err != nil {
-				makeError("Unable to read manifest for tag")
+				makeError("Unable to read manifest for tag", 3)
 			}
 
 			err = hub.DeleteManifest(repo, digest)
 			if err != nil {
 				fmt.Println(err)
-				makeError("Failed to delete tag")
+				makeError("Failed to delete tag, ignoring error", -1)
 			} else {
 				fmt.Println("Tag %s:%s deleted", repo, tag)
 			}
